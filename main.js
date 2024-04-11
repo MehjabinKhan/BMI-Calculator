e346be1a3902d6c5209380858fe6d6c5fdd238ac
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-04-11 19:46:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+// BMI Calculator
+// Calculating Self BMI
+let weightInKg = 40;
+let heightInMeters = 4.5;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`Your BMI is ${bmi}.`);
+// Calculating My Father BMI
+let weightInKg1 = 70;
+let heightInMeters1 = 5.2;
+let bmi1 = weightInKg1 / (heightInMeters1 * heightInMeters1);
+console.log(`Your Father BMI is ${bmi1}.`);
+// Calculating Brother BMI
+let weightInKg2 = 50;
+let heightInMeters2 = 5.5;
+let bmi2 = weightInKg2 / (heightInMeters2 * heightInMeters2);
+console.log(`Brother's BMI is ${bmi2}.`);
